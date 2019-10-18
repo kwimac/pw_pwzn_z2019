89caeafaf9ed014e9232dc93145fca4e2c38712d
Kicpero <kacper_raciborski@op.pl>
2019-10-18 10:17:14 +0200

    Naprawa

--- lab_2/tasks/task_2.py
+++ lab_2/tasks/task_2.py
@@ -1,31 +1,29 @@
 def stack_operation(stack_commands):
-    """
-    Funkcja przyjmuję listę jedno i dwu elementowych krotek - operacji na stosie.
-    Pierwszy element krotki to operacja, drugi wartość (opcjonalnie). Operacje:
-    push - dodaj element do stosu
-    pop - usuń element ze stosu
-    show_max - wypisz maksymalny element w stosie
-    Uzupełnij funkcje tak, by dla podanej zwróciła ciąg maksymalnych elementów (zgodny z liczbą operacj 3).
-
-    :param stack_commands: List of tuples of stack commands.
-    :type stack_commands: list
-    :return: List of outputs from commands.
-    :rtype: list
-    """
-    pass
+    tab_pom = []
+    for val in stack_commands:
+        if val[0] is 'pop':
+            print("ROBIE POP")
+            tab_pom.pop()
+        elif val[0] is 'push':
+            print("ROBIĘ PUSH")
+            tab_pom.append(val[1])
+        elif val[0] is 'show_max':
+            print(f'max:{max(tab_pom)}')
+    print(tab_pom)
+    return tab_pom
 
 
 if __name__ == "__main__":
     commands = [
         ('push', 97),
         ('pop',),
-        ('push', 20), 
-        ('pop',), 
-        ('push', 26), 
-        ('push', 20), 
-        ('pop',), 
-        ('show_max',), 
-        ('push', 91), 
+        ('push', 20),
+        ('pop',),
+        ('push', 26),
+        ('push', 20),
+        ('pop',),
+        ('show_max',),
+        ('push', 91),
         ('show_max',)
     ]
     assert stack_operation(commands) == [26, 91]
