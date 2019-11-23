5eb188855c4b18ae406fd769e1b2d59ce97051b1
zuzanna-mackiewicz <zuzia.mack@gmail.com>
2019-11-23 02:14:51 +0100

    'rozwiazanie_task2'

--- lab_6/tasks/task_2.py
+++ lab_6/tasks/task_2.py
@@ -11,11 +11,33 @@ dwuelementową tuplę zawierającą liczbę poprawnych wierszy:
 """
 import re
 
-
 def check_animal_list(file_path):
-    pass
 
+    with open(file_path) as file_:
+
+        f_pattern = r'^[\da-f]{8}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{12}_F_[\d]\.[\d]{3}[e][\-\+][\d]{2}$'
+        m_pattern = r'^[\da-f]{8}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{12}_M_[\d]\.[\d]{3}[e][\-\+][\d]{2}$'
+
+        f_count = 0
+        m_count = 0
+
+        for line in file_.readlines():
+
+            if bool(re.fullmatch(f_pattern, line.strip())):
+                f_count += 1
+            if bool(re.fullmatch(m_pattern, line.strip())):
+                m_count += 1
+
+        # print(f_count, m_count)
+        return f_count, m_count
 
 if __name__ == '__main__':
     assert check_animal_list('s_animals_sce.txt') == (2, 2)
-    assert check_animal_list('animals_sc_corrupted.txt') == (6, 0)
+    assert check_animal_list('animals_sc_corrupted.txt') == (5, 0)
+
+# Na podstawie Pana komentarza w konwersacji rozumiem, że program powinien uznawac wielkie litery w UUID jako blad.
+# Taka tez informacje znalazalam w internecie: "6.5.4 Software generating the hexadecimal representation of a UUID shall not use upper case letters."
+# Jednak w wielu zrodlach dopuszczalne jest uzycie rowniez wielkich liter. Wtedy f_pattern i m_pattern wygladalyby nastepujaco:
+#    f_pattern = r'^[\da-fA-F]{8}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{12}_F_[\d]\.[\d]{3}[e][\-\+][\d]{2}$'
+#    m_pattern = r'^[\da-fA-F]{8}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{12}_M_[\d]\.[\d]{3}[e][\-\+][\d]{2}$'
+
