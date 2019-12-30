ac6ed75ff81589ac07b47159bc5bae273f572774
Carlito <karol.schewe@gmail.com>
2019-12-30 20:18:17 +0100

    task1 - bez obslugi bledow

--- lab_10/tasks/task_1.py
+++ lab_10/tasks/task_1.py
@@ -1,10 +1,16 @@
 import requests
-
+from urllib.parse import urljoin
 
 def get_cities_woeid(query: str, timeout: float = 5.):
-    pass
+    API_URL = 'https://www.metaweather.com/api/'
+    location_url = urljoin(API_URL, 'location/search')
+    response = requests.get(location_url, params=dict(query=query)).json()
+    new_dict = {}
+    for i in response:
+        new_dict[i['title']] = i['woeid']
+    return new_dict
 
-
+# dodac obsluge bledow
 if __name__ == '__main__':
     assert get_cities_woeid('Warszawa') == {}
     assert get_cities_woeid('War') == {
