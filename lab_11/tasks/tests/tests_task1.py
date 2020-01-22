6bffbf56ca7daab6e8d11c8cbeba0b321faa2974
Carlito <karol.schewe>
2020-01-22 21:49:26 +0100

    zad11 - spóźnione

--- lab_11/tasks/tests/tests_task1.py
+++ lab_11/tasks/tests/tests_task1.py
@@ -7,3 +7,35 @@ from lab_11.tasks.tools.calculator import (
     NotNumberArgument,
     WrongOperation,
 )
+is_correct_math = [("+", 2, 2, 4),("-", 2, 2, 0),("*", 2, 2, 4),("/", 2, 2, 1)]
+is_correct_error = [("o", 1, 2, WrongOperation),("/", 69, None, EmptyMemory),("-", "12", 2, NotNumberArgument),("/", 1, 0.0, CalculatorError)]
+
+
+@pytest.fixture(scope="function")
+def calculator():
+    return Calculator()
+
+@pytest.mark.parametrize("operator,arg1,arg2,outcome", is_correct_math)
+def test_is_correct_math(operator, arg1, arg2, outcome, calculator):
+    val = calculator.run(operator, arg1, arg2)
+    assert val == outcome
+
+@pytest.mark.parametrize("operator,arg1,arg2,error", is_correct_error)
+def test_is_correct_error_raised(operator, arg1, arg2, error, calculator):
+    with pytest.raises(error):
+        calculator.run(operator, arg1, arg2)
+
+def test_memory_functionality(calculator: "Calculator"):
+    with pytest.raises(EmptyMemory):
+        calculator.in_memory()
+    with pytest.raises(EmptyMemory):
+        calculator.memory
+    calculator.run("*", 2, 2)
+    calculator.memorize()
+    assert calculator.memory == 4
+    assert calculator._short_memory == calculator.memory
+    calculator.clean_memory()
+    with pytest.raises(EmptyMemory):
+        calculator.in_memory()
+    with pytest.raises(EmptyMemory):
+        calculator.memory
--- lab_11/tasks/tests/tests_task2.py
+++ lab_11/tasks/tests/tests_task2.py
@@ -1,5 +1,27 @@
 import pytest
-
+import unittest.mock
 from lab_11.tasks.tools.metaweather import (
-    get_metaweather
+    get_metaweather,
+    get_cities_woeid
 )
+import requests
+
+
+
+def test_get_cities_woeid():
+    mock_response = unittest.mock.MagicMock()
+    mock_response.json.return_value = [{"title": "Warsaw", "woeid": 523920},{"title": "Newark", "woeid": 2459269}]
+    mock_response.status = 200
+
+    with unittest.mock.patch("requests.get", return_value=mock_response) as mock:
+        assert get_cities_woeid("War") == {
+            "Warsaw": 523920,
+            "Newark": 2459269
+        }
+
+
+def test_timeout_exception():
+
+    with unittest.mock.patch("requests.get", side_effect=requests.exceptions.Timeout()) as mock:
+        with pytest.raises(requests.exceptions.Timeout):
+            get_cities_woeid("Warszawa", 0.1)
